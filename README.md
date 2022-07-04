6942cb74708a31facb100cb198bb1be028bedc78
Artem Bychkov <artem.b91@mail.ru>
2022-07-04 13:22:16 +0300

    add Even game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ArtemB91/java-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/ArtemB91/java-project-lvl1/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/7f35f6e035d0a0c57554/maintainability)](https://codeclimate.com/github/ArtemB91/java-project-lvl1/maintainability)
+[![asciicast](https://asciinema.org/a/cYJPnyh56EUBmhJkk8HiLDQwm.svg)](https://asciinema.org/a/cYJPnyh56EUBmhJkk8HiLDQwm)
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greet();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        System.out.println("Your choice: " + choice);
+
+        switch (choice) {
+            case 1:
+                Cli.greet();
+                break;
+            case 2:
+                EvenGame.playEvenGame();
+                break;
+            default:
+                break;
+        }
+
     }
 }
