c121ed990117385235a18bedb8517f6acd0eb6fe
Artem Bychkov <artem.b91@mail.ru>
2022-07-11 13:28:07 +0300

    refactor delete unused import

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -3,8 +3,6 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
-import java.util.Random;
-
 public class Prime {
 
     private static final int MAX_NUMBER = 1000;
